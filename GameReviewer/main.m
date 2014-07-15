a08e8469bd1f2750d90ca20bc7d0fe988ed079fa
so30 <sortega@aurigae.com>
2014-07-15 12:14:25 +0200

    Initial Commit

---
+++ GameReviewer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GameReviewer
+//
+//  Created by sergio on 15/07/14.
+//  Copyright (c) 2014 SOM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GRAppDelegate class]));
+    }
+}
---
+++ GameReviewerTests/GameReviewerTests.m
@@ -0,0 +1,34 @@
+//
+//  GameReviewerTests.m
+//  GameReviewerTests
+//
+//  Created by sergio on 15/07/14.
+//  Copyright (c) 2014 SOM. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GameReviewerTests : XCTestCase
+
+@end
+
+@implementation GameReviewerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
